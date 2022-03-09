b37039a84e6b9aa2b4a011560e4fd6dddbaa8eea
David Plowman <david.plowman@raspberrypi.com>
2022-03-09 15:50:37 +0000

    Encode the stream named picam2.encode_stream_name, which might not be "main"

--- picamera2.py
+++ picamera2.py
@@ -455,7 +455,7 @@ class Picamera2:
             display_request.acquire()
 
             if self._encoder is not None:
-                stream = self.stream_map["main"]
+                stream = self.stream_map[self.encode_stream_name]
                 self._encoder.encode(stream, display_request)
 
             # See if any actions have been queued up for us to do here.
@@ -729,9 +729,10 @@ class Picamera2:
             raise RuntimeError("No video stream found")
         if self.encoder is None:
             raise RuntimeError("No encoder specified")
-        self.encoder.width, self.encoder.height = streams['main']['size']
-        self.encoder.format = streams['main']['format']
-        self.encoder.stride = streams['main']['stride']
+        name = self.encode_stream_name
+        self.encoder.width, self.encoder.height = streams[name]['size']
+        self.encoder.format = streams[name]['format']
+        self.encoder.stride = streams[name]['stride']
         self.encoder._start()
 
     def stop_encoder(self):
