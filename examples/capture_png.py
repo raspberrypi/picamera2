c29c0a250346719cf52daa7c9faf202f25ea2cd3
Ian Black <blackia@oregonstate.edu>
2022-03-09 18:02:24 -0800

    Updated to use new repo structure.

--- examples/capture_png.py
+++ examples/capture_png.py
@@ -2,17 +2,20 @@
 
 # Capture a PNG while still running in the preview mode.
 
-from qt_gl_preview import *
-from picamera2 import *
+from picamera2.picamera2 import *
 import time
 
 picam2 = Picamera2()
-preview = QtGlPreview(picam2)
+picam2.start_preview('QT')
 
 preview_config = picam2.preview_configuration(main={"size": (800, 600)})
 picam2.configure(preview_config)
 
-picam2.start()
+picam2.start_camera()
 time.sleep(2)
 
 picam2.capture_file("test.png")
+
+print("Sleeping for 10 seconds...")
+time.sleep(10)
+picam2.close_camera()
