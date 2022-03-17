38a59c91b4e1ccb5fdf45fdb770695941c569387
David Plowman <david.plowman@raspberrypi.com>
2022-03-17 10:45:03 +0000

    Improve shutdown of QtGlPreview window
    In particular this fixes the problem that you couldn't create a second
    one after closing down the first.
    
    I've also added a test to verify that this now works and added it to
    the automatic test set.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- picamera2/previews/qt_gl_preview.py
+++ picamera2/previews/qt_gl_preview.py
@@ -24,7 +24,6 @@ class QtGlPreview:
         atexit.unregister(self.stop)
         self.qpicamera2.picamera2.asynchronous = False
         # Again, all necessary to keep Qt quiet.
-        del self.qpicamera2.camera_notifier
         del self.qpicamera2
         del self.app
 
