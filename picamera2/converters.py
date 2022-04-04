fc623eb1234127c557785d17e22fa85bb7136bf3
Ian Black <blackia@oregonstate.edu>
2022-04-04 12:53:43 -0700

    Added rgb array to filetype converters.
    The user mush supply the numpy array of rgb values and a filename. Inclusion of a save_dir is optional. If no save_dir is specified, the image is saved to the current working directory if it is not a path.

--- picamera2/converters.py
+++ picamera2/converters.py
@@ -1,10 +1,52 @@
 import numpy as np
+import os
+from PIL import Image
+
+YUV2RGB_JPEG      = np.array([[1.0,   1.0,   1.0  ], [0.0, -0.344, 1.772], 
+                              [1.402, -0.714, 0.0]])
+YUV2RGB_SMPTE170M = np.array([[1.164, 1.164, 1.164], [0.0, -0.392, 2.017], 
+                              [1.596, -0.813, 0.0]])
+YUV2RGB_REC709    = np.array([[1.164, 1.164, 1.164], [0.0, -0.213, 2.112], 
+                              [1.793, -0.533, 0.0]])
+
+def _rgb_to_fmt(image_array,filename,save_dir,fmt):
+    filepath,filename = os.path.split(filename)
+    basename,ext = os.path.splitext(filename)
+    if fmt not in basename:
+        filename = basename + fmt
+    if save_dir is not None:
+        abs_path = os.path.normpath(os.path.join(save_dir,filename))
+    else:
+        abs_path = filename #If the filename is not a path, then it will save in the current working directory.
+    if '.npy' in filename:
+        np.save(abs_path,image_array)
+    else:
+        img = Image.fromarray(image_array)
+        if '.jpg' in filename:
+            img = img.convert('RGB')
+        img.save(abs_path)
+    return abs_path
+
+def rgb2npy(image_array,filename,save_dir=None):
+    fmt = '.npy'
+    img = _rgb_to_fmt(image_array,filename,save_dir,fmt)
+    return img
+
+def rgb2jpg(image_array,filename,save_dir = None,exif_metadata = {}):
+    fmt = '.jpg'
+    img = _rgb_to_fmt(image_array,filename,save_dir,fmt)
+    return img
 
 
-YUV2RGB_JPEG      = np.array([[1.0,   1.0,   1.0  ], [0.0, -0.344, 1.772], [1.402, -0.714, 0.0]])
-YUV2RGB_SMPTE170M = np.array([[1.164, 1.164, 1.164], [0.0, -0.392, 2.017], [1.596, -0.813, 0.0]])
-YUV2RGB_REC709    = np.array([[1.164, 1.164, 1.164], [0.0, -0.213, 2.112], [1.793, -0.533, 0.0]])
+def rgb2png(image_array,filename,save_dir = None):
+    fmt = '.png'
+    img = _rgb_to_fmt(image_array,filename,save_dir,fmt)
+    return img
 
+def rgb2tif(image_array,filename,save_dir = None):
+    fmt = '.tif'
+    img = _rgb_to_fmt(image_array,filename,save_dir,fmt)
+    return img
 
 def YUV420_to_RGB(YUV_in, size, matrix=YUV2RGB_JPEG, rb_swap=True, final_width=0):
     """Convert a YUV420 image to an interleaved RGB image of half resolution. The
