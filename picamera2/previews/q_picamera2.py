9e1beffb16a6ce2f773e87449930aaeabd2a5d0b
Jason Lane <53111861+jlprojects@users.noreply.github.com>
2022-04-18 17:30:07 +0100

    Convert QPicamera widget to QGraphicsView to allow dynamic resizing of preview

--- picamera2/previews/q_picamera2.py
+++ picamera2/previews/q_picamera2.py
@@ -1,19 +1,20 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import pyqtSlot, QSocketNotifier
+from PyQt5.QtCore import Qt, pyqtSlot, QSocketNotifier, QRectF
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel
-from PIL import Image
-from PIL.ImageQt import ImageQt
+from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene
 import numpy as np
 
 
-class QPicamera2(QWidget):
-    def __init__(self, picam2, parent=None, width=640, height=480):
+class QPicamera2(QGraphicsView):
+    def __init__(self, picam2, parent=None):
         super().__init__(parent=parent)
         self.picamera2 = picam2
-        self.label = QLabel(self)
-        self.label.resize(width, height)
+
+        self.pixmap = None
         self.overlay = None
-        self.painter = QtGui.QPainter()
+        self.scene = QGraphicsScene()
+        self.setScene(self.scene)
+
         self.camera_notifier = QSocketNotifier(self.picamera2.camera_manager.efd,
                                                QtCore.QSocketNotifier.Read,
                                                self)
@@ -21,19 +22,25 @@ class QPicamera2(QWidget):
 
     def set_overlay(self, overlay):
         if overlay is not None:
-            # Better to resize the overlay here rather than in the rendering loop.
-            orig = overlay
             overlay = np.ascontiguousarray(overlay)
             shape = overlay.shape
-            size = self.label.size()
-            if orig is overlay and shape[1] == size.width() and shape[0] == size.height():
-                # We must be sure to copy the data even when no one else does!
-                overlay = overlay.copy()
-            overlay = QtGui.QImage(overlay.data, shape[1], shape[0], QtGui.QImage.Format_RGBA8888)
-            if overlay.size() != self.label.size():
-                overlay = overlay.scaled(self.label.size())
-
-        self.overlay = overlay
+            qim = QtGui.QImage(overlay.data, shape[1], shape[0],
+                               QtGui.QImage.Format_RGBA8888)
+            if qim.size() != self.scene.sceneRect().size():
+                # Resize the overlay
+                qim = qim.scaled(self.scene.sceneRect().size().toSize())
+            pix = QtGui.QPixmap(qim)
+            if self.overlay is None:
+                # Need to add the overlay to the scene
+                self.overlay = self.scene.addPixmap(pix)
+                self.overlay.setZValue(100)
+            else:
+                # Just update it
+                self.overlay.setPixmap(pix)
+        elif self.overlay is not None:
+            # Remove overlay
+            self.scene.removeItem(self.overlay)
+            self.overlay = None
 
     @pyqtSlot()
     def handle_requests(self):
@@ -42,15 +49,18 @@ class QPicamera2(QWidget):
             return
 
         if self.picamera2.display_stream_name is not None:
-            # This all seems horribly expensive. Pull request welcome if you know a better way!
-            size = self.label.size()
-            img = request.make_image(self.picamera2.display_stream_name, size.width(), size.height())
-            qim = ImageQt(img)
-            self.painter.begin(qim)
-            overlay = self.overlay
-            if overlay is not None:
-                self.painter.drawImage(0, 0, overlay)
-            self.painter.end()
-            pix = QtGui.QPixmap.fromImage(qim)
-            self.label.setPixmap(pix)
+            img = request.make_array(self.picamera2.display_stream_name)
+            img = np.ascontiguousarray(img[..., :3])
+            shape = img.shape
+            qim = QtGui.QImage(img.data, shape[1], shape[0],
+                               QtGui.QImage.Format_RGB888)
+            pix = QtGui.QPixmap(qim)
+            if self.pixmap is None:
+                # Add the pixmap to the scene
+                self.pixmap = self.scene.addPixmap(pix)
+            else:
+                # Update pixmap
+                self.pixmap.setPixmap(pix)
+            self.fitInView(self.sceneRect(), Qt.KeepAspectRatio)
+
         request.release()
