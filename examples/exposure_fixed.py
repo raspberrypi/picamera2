0a7a68d87ad6acdb8a7253edff39e480e5ce1112
Ian Black <blackia@oregonstate.edu>
2022-05-12 09:33:10 -0700

    Changes to start_, start, and configure_.
    Removed controls option from start functions and implemented set_controls in configure_. Updated relevant examples to match.

--- examples/exposure_fixed.py
+++ examples/exposure_fixed.py
@@ -7,9 +7,9 @@ import time
 
 picam2 = Picamera2()
 picam2.start_preview(Preview.QTGL)
-
-preview_config = picam2.preview_configuration()
+controls = {"ExposureTime": 10000, "AnalogueGain": 1.0}
+preview_config = picam2.preview_configuration(controls = controls)
 picam2.configure(preview_config)
 
-picam2.start({"ExposureTime": 10000, "AnalogueGain": 1.0})
+picam2.start()
 time.sleep(5)
