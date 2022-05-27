2e79aeaff1b66c5c74890cb92452172848ab7f3a
Jason Lane <53111861+jlprojects@users.noreply.github.com>
2022-05-27 17:01:04 +0100

    Implement QPicamera2 widget using QGraphicsView
    Pretty much a complete rewrite.
    
    Signed-off-by: Jason Lane <53111861+jlprojects@users.noreply.github.com>
    Reviewed-by: David Plowman <david.plowman@raspberrypi.com>

--- picamera2/previews/q_picamera2.py
+++ picamera2/previews/q_picamera2.py
@@ -1,13 +1,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import pyqtSlot, QSocketNotifier, Qt, pyqtSignal
-from PyQt5.QtGui import QColor, QPainter, QPixmap, QImage
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel
-from PIL import Image
-from PIL.ImageQt import ImageQt
+from PyQt5.QtCore import Qt, pyqtSlot, pyqtSignal, QSocketNotifier, QSize, QRect, QRectF
+from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene
+from PyQt5.QtGui import QBrush, QColor, QImage, QPixmap, QTransform
 import numpy as np
 
 
-class QPicamera2(QWidget):
+class QPicamera2(QGraphicsView):
     done_signal = pyqtSignal()
     update_overlay_signal = pyqtSignal(object)
 
@@ -15,106 +13,110 @@ class QPicamera2(QWidget):
         super().__init__(parent=parent)
         self.picamera2 = picam2
         picam2.have_event_loop = True
-        self.bg_colour = bg_colour
         self.keep_ar = keep_ar
-        self.pil_img = None
-        self.pil_img_resized = None
-        self.label = QLabel(self)
-        self.label.resize(width, height)
+        self.image_size = None
+        self.last_rect = QRect(0, 0, 0, 0)
+
+        self.size = QSize(width, height)
+        self.pixmap = None
         self.overlay = None
-        self.overlay_resized = None
-        self.painter = QPainter()
+        self.scene = QGraphicsScene()
+        self.setScene(self.scene)
+        self.setBackgroundBrush(QBrush(QColor(*bg_colour)))
+        self.resize(width, height)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.enabled = True
+
         self.update_overlay_signal.connect(self.update_overlay)
         self.camera_notifier = QSocketNotifier(self.picamera2.camera_manager.efd,
                                                QSocketNotifier.Read, self)
         self.camera_notifier.activated.connect(self.handle_requests)
 
     def cleanup(self):
-        del self.label
+        del self.scene
+        del self.overlay
         del self.camera_notifier
 
     def signal_done(self, picamera2):
         self.done_signal.emit()
 
+    def image_dimensions(self):
+        # The dimensions of the camera images we're displaying.
+        camera_config = self.picamera2.camera_config
+        if camera_config is not None and camera_config['display'] is not None:
+            # This works even before we receive any camera images.
+            size = self.picamera2.stream_map[camera_config['display']].configuration.size
+        elif self.image_size is not None:
+            # If the camera is unconfigured, stick with the last size (if available).
+            size = self.image_size
+        else:
+            # Otherwise pretend the image fills everything.
+            rect = self.viewport().rect()
+            size = rect.width(), rect.height()
+        self.image_size = size
+        return size
+
     def set_overlay(self, overlay):
+        camera_config = self.picamera2.camera_config
+        if camera_config is None:
+            raise RuntimeError("Camera must be configured before using set_overlay")
+
+        new_pixmap = None
         if overlay is not None:
             overlay = np.copy(overlay, order='C')
             shape = overlay.shape
-            overlay = QImage(overlay.data, shape[1], shape[0], QImage.Format_RGBA8888)
-        self.update_overlay_signal.emit(overlay)
+            qim = QImage(overlay.data, shape[1], shape[0], QImage.Format_RGBA8888)
+            new_pixmap = QPixmap(qim)
+            # No scaling here - we leave it to fitInView to set that up.
+        self.update_overlay_signal.emit(new_pixmap)
 
-    def update_overlay(self, overlay):
-        self.overlay = overlay
-        self.overlay_resized = None
-        self.update()
-
-    def recalculate_viewport(self):
-        size = self.label.size()
-        win_w = size.width()
-        win_h = size.height()
-
-        stream_map = self.picamera2.stream_map
-        camera_config = self.picamera2.camera_config
-        if not self.keep_ar or camera_config is None or camera_config['display'] is None:
-            return 0, 0, win_w, win_h
-
-        im_w, im_h = stream_map[camera_config['display']].configuration.size
-        if im_w * win_h > win_w * im_h:
-            im_win_w = win_w
-            im_win_h = win_w * im_h // im_w
+    @pyqtSlot(object)
+    def update_overlay(self, pix):
+        if pix is None:
+            # Delete overlay if present
+            if self.overlay is not None:
+                self.scene.removeItem(self.overlay)
+                self.overlay = None
+                return
+        elif self.overlay is None:
+            # Need to add the overlay to the scene
+            self.overlay = self.scene.addPixmap(pix)
+            self.overlay.setZValue(100)
         else:
-            im_win_h = win_h
-            im_win_w = win_h * im_w // im_h
-        im_win_x = (win_w - im_win_w) // 2
-        im_win_y = (win_h - im_win_h) // 2
-        return (im_win_x, im_win_y, im_win_w, im_win_h)
+            # Just update it
+            self.overlay.setPixmap(pix)
+        self.fitInView()
 
-    def paintEvent(self, event):
-        # This all seems horribly expensive. Pull request welcome if you know a better way!
-        picam2 = self.picamera2
-        size = self.label.size()
-        win_w = size.width()
-        win_h = size.height()
-        im_win_x, im_win_y, im_win_w, im_win_h = self.recalculate_viewport()
+    @pyqtSlot(bool)
+    def set_enabled(self, enabled):
+        self.enabled = enabled
 
-        # Check if the pil_img needs to be resized again if the window has changed.
-        if self.pil_img_resized:
-            if self.pil_img_resized.width != im_win_w or self.pil_img_resized.height != im_win_h:
-                self.pil_img_resized = None
+    def fitInView(self):
+        # Reimplemented fitInView to remove fixed border
+        image_w, image_h = self.image_dimensions()
+        rect = QRectF(0, 0, image_w, image_h)
+        self.setSceneRect(rect)
+        # I get one column of background peeping through on the right without this:
+        viewrect = self.viewport().rect().adjusted(0, 0, 1, 1)
+        self.resetTransform()
+        factor_x = viewrect.width() / image_w
+        factor_y = viewrect.height() / image_h
+        if self.keep_ar:
+            factor_x = min(factor_x, factor_y)
+            factor_y = factor_x
+        self.scale(factor_x, factor_y)
 
-        # Remake the cached version that fits in the window.
-        if self.pil_img_resized is None and self.pil_img:
-            if self.pil_img.width == im_win_w and self.pil_img.height == im_win_h:
-                self.pil_img_resized = self.pil_img
-            else:
-                self.pil_img_resized = self.pil_img.resize((im_win_w, im_win_h))
-
-        # The cached resized overlay needs to be resized again if the window has changed.
-        if self.overlay_resized:
-            if self.overlay_resized.width() != im_win_w or self.overlay_resized.height() != im_win_h:
-                self.overlay_resized = None
-
-        # Remake the cached resized overlay.
-        if self.overlay and self.overlay_resized is None:
-            self.overlay_resized = self.overlay.scaled(im_win_w, im_win_h)
-
-        # Now render everything onto the label.
-        pixmap = QPixmap(win_w, win_h)
-        self.painter.begin(pixmap)
-        self.painter.fillRect(0, 0, win_w, win_h, QColor.fromRgb(*self.bg_colour))
-
-        if self.pil_img_resized:
-            self.painter.drawImage(im_win_x, im_win_y, ImageQt(self.pil_img_resized))
-
-        if self.overlay_resized:
-            self.painter.drawImage(im_win_x, im_win_y, self.overlay_resized)
-
-        self.painter.end()
-        self.label.setPixmap(pixmap)
+        # This scales the overlay to be on top of the camera image.
+        if self.overlay:
+            rect = self.overlay.boundingRect()
+            self.overlay.resetTransform()
+            factor_x = image_w / rect.width()
+            factor_y = image_h / rect.height()
+            self.overlay.setTransform(QTransform.fromScale(factor_x, factor_y), True)
 
     def resizeEvent(self, event):
-        self.label.resize(self.width(), self.height())
-        self.update()
+        self.fitInView()
 
     @pyqtSlot()
     def handle_requests(self):
@@ -122,10 +124,21 @@ class QPicamera2(QWidget):
         if not request:
             return
 
-        self.pil_img = None
-        self.pil_img_resized = None
-        if self.picamera2.display_stream_name is not None:
-            self.pil_img = request.make_image(self.picamera2.display_stream_name)
-
+        if self.enabled and self.picamera2.display_stream_name is not None:
+            img = request.make_array(self.picamera2.display_stream_name)
+            img = np.ascontiguousarray(img[..., :3])
+            shape = img.shape
+            qim = QImage(img.data, shape[1], shape[0], QImage.Format_RGB888)
+            pix = QPixmap(qim)
+            # Add the pixmap to the scene if there wasn't one, or replace it if the images have
+            # changed size.
+            if self.pixmap is None or pix.rect() != self.last_rect:
+                if self.pixmap:
+                    self.scene.removeItem(self.pixmap)
+                self.last_rect = pix.rect()
+                self.pixmap = self.scene.addPixmap(pix)
+                self.fitInView()
+            else:
+                # Update pixmap
+                self.pixmap.setPixmap(pix)
         request.release()
-        self.update()
