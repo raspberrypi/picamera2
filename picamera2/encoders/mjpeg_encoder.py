1269565d4a70ac06df06993f9007441e13f541f2
chrisruk <christopher.richardson@raspberrypi.org>
2022-06-15 14:17:32 +0100

    Docstrings for MJPEGEncoder
    Signed-off-by: Chris Richardson <christopher.richardson@raspberrypi.org>

--- picamera2/encoders/mjpeg_encoder.py
+++ picamera2/encoders/mjpeg_encoder.py
@@ -1,8 +1,17 @@
+"""MJPEG encoder functionality utilising V4L2"""
+
 from v4l2 import *
 
 from picamera2.encoders.v4l2_encoder import V4L2Encoder
 
 
 class MJPEGEncoder(V4L2Encoder):
+    """MJPEG encoder utilsing V4L2 functionality"""
+
     def __init__(self, bitrate):
+        """Creates MJPEG encoder
+
+        :param bitrate: Bitrate
+        :type bitrate: int
+        """
         super().__init__(bitrate, V4L2_PIX_FMT_MJPEG)
