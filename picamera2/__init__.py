c1498170bf2b312f86c043f881d757fde4c61a89
David Plowman <david.plowman@raspberrypi.com>
2022-06-16 10:49:34 +0100

    Improve repr and eq functions for Transforms and ColorSpaces
    The supplied repr functions are not very helpful, they just show you a
    hex number. So we'll override them with the str method instead.
    
    The new eq functions just test the constituent enum fields, which is
    what I think most users would expect.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- picamera2/__init__.py
+++ picamera2/__init__.py
@@ -1,3 +1,21 @@
 from .converters import YUV420_to_RGB
 from .picamera2 import Picamera2, Preview
 from .request import CompletedRequest, MappedArray
+
+import libcamera
+
+
+def libcamera_transforms_eq(t1, t2):
+    return t1.hflip == t2.hflip and t1.vflip == t2.vflip and t1.transpose == t2.transpose
+
+
+def libcamera_colour_spaces_eq(c1, c2):
+    return c1.primaries == c2.primaries and c1.transferFunction == c2.transferFunction and \
+        c1.ycbcrEncoding == c2.ycbcrEncoding and c1.range == c2.range
+
+
+libcamera.Transform.__repr__ = libcamera.Transform.__str__
+libcamera.Transform.__eq__ = libcamera_transforms_eq
+
+libcamera.ColorSpace.__repr__ = libcamera.ColorSpace.__str__
+libcamera.ColorSpace.__eq__ = libcamera_colour_spaces_eq
