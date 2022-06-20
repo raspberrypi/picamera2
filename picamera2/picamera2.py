1bf82ce4498dbc1a76b01370854bb310289b917e
David Plowman <david.plowman@raspberrypi.com>
2022-06-20 16:43:55 +0100

    Add an optional config parameter to start_recording
    This just saves you having to call configure separately if you don't
    want to.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- picamera2/picamera2.py
+++ picamera2/picamera2.py
@@ -1227,7 +1227,7 @@ class Picamera2:
             raise RuntimeError("Must pass encoder instance")
         self._encoder = value
 
-    def start_recording(self, encoder, output) -> None:
+    def start_recording(self, encoder, output, config=None) -> None:
         """Start recording a video using the given encoder and to the given output.
 
         Output may be a string in which case the correspondingly named file is opened.
@@ -1237,6 +1237,8 @@ class Picamera2:
         :param output: FileOutput object
         :type output: FileOutput
         """
+        if config is not None:
+            self.configure(config)
         if isinstance(output, str):
             output = FileOutput(output)
         encoder.output = output
