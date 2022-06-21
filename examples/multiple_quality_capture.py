d0f826a54fc820508dcb55ece7761dc1161b8830
David Plowman <david.plowman@raspberrypi.com>
2022-06-21 14:23:49 +0100

    Add an example showing how to capture video with different quality settings
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

---
+++ examples/multiple_quality_capture.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import time
+
+from picamera2.encoders import H264Encoder, Quality
+from picamera2 import Picamera2
+
+picam2 = Picamera2()
+video_config = picam2.create_video_configuration()
+picam2.configure(video_config)
+
+encoder = H264Encoder()
+
+picam2.start_recording(encoder, 'low.h264', quality=Quality.VERY_LOW)
+print(encoder._bitrate)
+time.sleep(5)
+picam2.stop_recording()
+
+picam2.start_recording(encoder, 'medium.h264', quality=Quality.MEDIUM)
+print(encoder._bitrate)
+time.sleep(5)
+picam2.stop_recording()
+
+picam2.start_recording(encoder, 'high.h264', quality=Quality.VERY_HIGH)
+print(encoder._bitrate)
+time.sleep(5)
+picam2.stop_recording()
