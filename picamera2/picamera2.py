cd27745900fbf027791af6b7cb6f49b48d241663
David Plowman <david.plowman@raspberrypi.com>
2022-06-22 10:33:08 +0100

    Add convenience "start the camera and capture timelapse images" function
    start_camera_and_capture_files makes it easier for users to configure
    and start the camera automatically, and capture a series of timelapse
    images. It does nothing that you can't do with a for loop, it is
    merely a convenience.
    
    A similar start_camera_and_capture_file function is added to capture a
    single image.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- picamera2/picamera2.py
+++ picamera2/picamera2.py
@@ -9,6 +9,7 @@ import tempfile
 import threading
 from enum import Enum
 from typing import List
+import time
 
 import libcamera
 import numpy as np
@@ -1275,3 +1276,83 @@ class Picamera2:
             if overlay.ndim != 3 or overlay.shape[2] != 4:
                 raise RuntimeError("Overlay must be a 4-channel image")
         self._preview.set_overlay(overlay)
+
+    def start_and_capture_files(self, name="image{:03d}.jpg", initial_delay=1, preview_mode="preview", capture_mode="still", num_files=1, delay=1, show_preview=True):
+        """
+        This function makes capturing multiple images more conenient, but should only be used in
+        command line line applications (not from a Qt application, for example). If will configure
+        the camera as requested and start it, switching between preview and still modes for
+        capture. It supports the following parameters (all optional):
+
+        name - name of the files to which to save the images. If more than one image is to be
+            captured then it should feature a format directive that will be replaced by a counter.
+
+        initial_delay - any time delay (in seconds) before the first image is captured. The camera
+            will run in preview mode during this period. The default time is 1s.
+
+        preview_mode - the camera mode to use for the preview phase (defaulting to the
+            Picamera2 object's preview_configuration field).
+
+        capture_mode - the camera mode to use to capture the still images (defaulting to the
+            Picamera2 object's still_configuration field).
+
+        num_files - number of files to capture (default 1).
+
+        delay - the time delay for every capture after the first (default 1s).
+
+        show_preview - whether to show a preview window (default: yes). The preview window only
+            displays an image by default during the preview phase, so if captures are back-to-back
+            with delay zero, then there may be no images shown. This parameter only has any
+            effect if a preview is not already running. If it is, it would have to be stopped first
+            (with the stop_preview method).
+        """
+        if self.started:
+            self.stop()
+        if delay:
+            # Show a preview between captures, so we will switch mode and back for each capture.
+            self.configure(preview_mode)
+            self.start(show_preview=show_preview)
+            for i in range(num_files):
+                time.sleep(initial_delay if i == 0 else delay)
+                self.switch_mode_and_capture_file(capture_mode, name.format(i))
+        else:
+            # No preview between captures, it's more efficient just to stay in capture mode.
+            if initial_delay:
+                self.configure(preview_mode)
+                self.start(show_preview=show_preview)
+                time.sleep(initial_delay)
+                self.switch_mode(capture_mode)
+            else:
+                self.configure(capture_mode)
+                self.start(show_preview=show_preview)
+            for i in range(num_files):
+                self.capture_file(name.format(i))
+                if i == num_files - 1:
+                    break
+                time.sleep(delay)
+        self.stop()
+
+    def start_and_capture_file(self, name="image.jpg", delay=1, preview_mode="preview", capture_mode="still", show_preview=True):
+        """
+        This function makes capturing a single image more conenient, but should only be used in
+        command line line applications (not from a Qt application, for example). If will configure
+        the camera as requested and start it, switching from the preview to the still mode for
+        capture. It supports the following parameters (all optional):
+
+        name - name of the file to which to save the images.
+
+        delay - any time delay (in seconds) before the image is captured. The camera
+            will run in preview mode during this period. The default time is 1s.
+
+        preview_mode - the camera mode to use for the preview phase (defaulting to the
+            Picamera2 object's preview_configuration field).
+
+        capture_mode - the camera mode to use to capture the still images (defaulting to the
+            Picamera2 object's still_configuration field).
+
+        show_preview - whether to show a preview window (default: yes). The preview window only
+            displays an image by default during the preview phase. This parameter only has any
+            effect if a preview is not already running. If it is, it would have to be stopped first
+            (with the stop_preview method).
+        """
+        self.start_and_capture_files(name=name, initial_delay=delay, preview_mode=preview_mode, capture_mode=capture_mode, num_files=1, show_preview=show_preview)
