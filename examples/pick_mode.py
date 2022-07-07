bede4393c3ea46067ed0a5e49a0f69f16d267b76
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2022-07-07 15:03:18 +0100

    Add example to demonstrate sensor_modes
    Add example pick_mode.py to demonstrate selecting an optimum format
    for high frame rate recording.
    
    Selects the highest framerate with a bit depth of 10, then records at
    this framerate.
    
    Signed-off-by: William Vinnicombe <william.vinnicombe@raspberrypi.com>

---
+++ examples/pick_mode.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+# Example of reading the available modes, and picking one with
+# the highest framerate and a raw bit depth of at least 10
+
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder, Quality
+from picamera2.outputs import FfmpegOutput
+import time
+
+
+picam2 = Picamera2()
+
+available_modes = picam2.sensor_modes
+min_bit_depth = 10
+available_modes = list(filter(lambda x: (x["bit_depth"] >= min_bit_depth), available_modes))
+available_modes.sort(key=lambda x: x["fps"], reverse=True)
+[print(i) for i in available_modes]
+chosen_mode = available_modes[0]
+
+picam2.video_configuration = picam2.create_video_configuration(
+    raw={"size": chosen_mode["size"], "format": chosen_mode["format"].format}
+)
+picam2.configure("video")
+
+encoder = H264Encoder()
+output = FfmpegOutput('test.mp4')
+
+# Set the fps
+fps = chosen_mode["fps"]
+picam2.set_controls({"FrameRate": fps})
+
+picam2.start_recording(encoder, output, quality=Quality.LOW)
+
+time.sleep(5)
+
+picam2.stop_recording()
