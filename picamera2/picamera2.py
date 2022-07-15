b661a6bd1ee453e54fa26204ae42e64d628356cf
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2022-07-15 12:57:29 +0100

    Add 8bit bayer formats to is_Bayer check
    Signed-off-by: William Vinnicombe <william.vinnicombe@raspberrypi.com>

--- picamera2/picamera2.py
+++ picamera2/picamera2.py
@@ -593,7 +593,8 @@ class Picamera2:
         return fmt in ("BGR888", "RGB888", "XBGR8888", "XRGB8888")
 
     def is_Bayer(self, fmt) -> bool:
-        return fmt in ("SBGGR10", "SGBRG10", "SGRBG10", "SRGGB10",
+        return fmt in ("SBGGR8", "SGBRG8", "SGRBG8", "SRGGB8",
+                       "SBGGR10", "SGBRG10", "SGRBG10", "SRGGB10",
                        "SBGGR10_CSI2P", "SGBRG10_CSI2P", "SGRBG10_CSI2P", "SRGGB10_CSI2P",
                        "SBGGR12", "SGBRG12", "SGRBG12", "SRGGB12",
                        "SBGGR12_CSI2P", "SGBRG12_CSI2P", "SGRBG12_CSI2P", "SRGGB12_CSI2P")
