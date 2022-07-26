a1c9ba4857f25240b7e4cdff7d9d5acea1b69f0a
David Plowman <david.plowman@raspberrypi.com>
2022-07-26 09:35:06 +0100

    Fix typo in examples/controls_3.py
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- examples/controls_3.py
+++ examples/controls_3.py
@@ -10,7 +10,7 @@ from picamera2.controls import Controls
 picam2 = Picamera2()
 picam2.start_preview(Preview.QTGL)
 
-preview_config = picam2.preview_configuration()
+preview_config = picam2.create_preview_configuration()
 picam2.configure(preview_config)
 
 picam2.start()
