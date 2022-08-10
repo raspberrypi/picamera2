7992ee26185be45a0e55729e06c21e1eea1f850b
chrisruk <christopher.richardson@raspberrypi.org>
2022-08-10 11:26:57 +0100

    Example for circular buffer
    In this example, there is no outputting of frames to a file,
    until .stop is called
    
    Signed-off-by: Chris Richardson <christopher.richardson@raspberrypi.org>

---
+++ examples/capture_circular_nooutput.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+import time
+
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import CircularOutput
+
+picam2 = Picamera2()
+fps = 30
+dur = 5
+micro = int((1 / fps) * 1000000)
+vconfig = picam2.create_video_configuration()
+vconfig['controls']['FrameDurationLimits'] = (micro, micro)
+picam2.configure(vconfig)
+encoder = H264Encoder()
+output = CircularOutput(buffersize=fps * dur, outputtofile=False)
+output.fileoutput = "file.h264"
+picam2.start_recording(encoder, output)
+time.sleep(dur)
+output.stop()
