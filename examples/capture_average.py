bf0fd2c2e1b5f29521aec5611012cb5fdb257a56
chrisruk <christopher.richardson@raspberrypi.org>
2022-08-24 10:54:45 +0100

    Example where we average multiple photos to reduce noise
    Signed-off-by: Chris Richardson <christopher.richardson@raspberrypi.org>

---
+++ examples/capture_average.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""Example comparing capturing a single photo vs capturing multiple photos and averaging to try to reduce noise"""
+
+import time
+
+import numpy as np
+from picamera2 import Picamera2, Preview
+from PIL import Image
+
+picam2 = Picamera2()
+picam2.start_preview(Preview.NULL)
+capture_config = picam2.create_still_configuration()
+picam2.configure(capture_config)
+
+picam2.start()
+time.sleep(2)
+
+with picam2.controls as ctrl:
+    ctrl.AnalogueGain = 1.0
+    ctrl.ExposureTime = 400000
+time.sleep(2)
+
+imgs = 20  # Capture 20 images to average
+sumv = None
+for i in range(imgs):
+    if sumv is None:
+        sumv = np.longdouble(picam2.capture_array())
+        img = Image.fromarray(np.uint8(sumv))
+        img.save("original.tif")
+    else:
+        sumv += np.longdouble(picam2.capture_array())
+
+img = Image.fromarray(np.uint8(sumv / imgs))
+img.save("averaged.tif")
