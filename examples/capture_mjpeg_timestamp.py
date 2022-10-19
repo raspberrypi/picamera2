9ab370385b7cceb4905fc379a12ecc5fe023c957
chrisruk <christopher.richardson@raspberrypi.org>
2022-10-19 12:02:52 +0100

    MJPEG timestamp example
    Signed-off-by: Chris Richardson <christopher.richardson@raspberrypi.org>

---
+++ examples/capture_mjpeg_timestamp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import time
+
+from picamera2 import Picamera2
+from picamera2.encoders import JpegEncoder
+
+picam2 = Picamera2()
+video_config = picam2.create_video_configuration(main={"size": (1920, 1080)})
+picam2.configure(video_config)
+
+picam2.start_preview()
+encoder = JpegEncoder(q=70)
+
+picam2.start_recording(encoder, 'test.mjpeg', pts='timestamp.txt')
+time.sleep(10)
+picam2.stop_recording()
