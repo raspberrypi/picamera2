13ab446b6f817eba0c2727f22a344aa6fed8890d
David Plowman <david.plowman@raspberrypi.com>
2022-11-02 18:43:25 +0000

    Add a test that runs two DRM previews at once
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

---
+++ tests/drm_multiple_test.py
@@ -0,0 +1,24 @@
+from picamera2 import Picamera2, Preview
+import time
+
+picam2a = Picamera2(0)
+picam2a.start_preview(Preview.DRM, x=1000, y=0)
+picam2a.start()
+time.sleep(1)
+
+picam2b = Picamera2(1)
+picam2b.start_preview(Preview.DRM, x=1000, y=500)
+picam2b.start()
+time.sleep(1)
+
+picam2a.close()
+picam2a = Picamera2(0)
+picam2a.start_preview(Preview.DRM, x=0, y=0)
+picam2a.start()
+time.sleep(1)
+
+picam2b.close()
+picam2b = Picamera2(1)
+picam2b.start_preview(Preview.DRM, x=0, y=500)
+picam2b.start()
+time.sleep(1)
