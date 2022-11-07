8f9a1a1834adf5dfae94df4e4cb57297ba02a62f
David Plowman <david.plowman@raspberrypi.com>
2022-11-07 16:32:54 +0000

    Add a simple multi camera preview test
    Test checks for and only does anything if there's more than one
    camera.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

---
+++ tests/multicamera.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import time
+
+from picamera2 import Picamera2, Preview
+
+if len(Picamera2.global_camera_info()) <= 1:
+    quit()
+
+picam2a = Picamera2(0)
+picam2a.configure(picam2a.create_preview_configuration())
+picam2a.start_preview(Preview.QTGL)
+
+picam2b = Picamera2(1)
+picam2b.configure(picam2b.create_preview_configuration())
+picam2b.start_preview(Preview.QT)
+
+picam2a.start()
+picam2b.start()
+
+time.sleep(2)
+print(picam2a.capture_metadata())
+time.sleep(2)
+print(picam2b.capture_metadata())
+picam2a.capture_file("testa.jpg")
+picam2b.capture_file("testb.jpg")
+
+picam2a.stop()
+picam2b.stop()
