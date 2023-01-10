5e477e26900ffcce37b2977fa4e99cdd945ecc47
David Plowman <david.plowman@raspberrypi.com>
2023-01-10 15:31:05 +0000

    Fix switch-mode tests for imx708
    It can produce images wider than the GPU can display, so reduce the
    maximum width in these cases.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- examples/switch_mode_2.py
+++ examples/switch_mode_2.py
@@ -16,7 +16,14 @@ picam2.start()
 time.sleep(2)
 picam2.stop()
 
-other_config = picam2.create_preview_configuration(main={"size": picam2.sensor_resolution}, buffer_count=3)
+size = picam2.sensor_resolution
+# GPU won't digest images wider than 4096 on a Pi 4.
+if size[0] > 4096:
+    height = size[1] * 4096 // size[0]
+    height -= height % 2
+    size = (4096, height)
+
+other_config = picam2.create_preview_configuration(main={"size": size}, buffer_count=2)
 picam2.configure(other_config)
 
 picam2.start()
