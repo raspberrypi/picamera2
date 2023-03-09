070e28fe7c21a0fc090a81cb388280fd64def227
Petros626 <62354721+Petros626@users.noreply.github.com>
2023-03-09 10:13:29 +0100

    Create capture_images_by_key.py
    I just created a simple example to take picutres with the keyboard. Pressing 'p' is taking a picture with 'q' you exit the script.
    The filename of the taken pictures are ordered as YearMonthDay-HourMinuteSecond.

---
+++ examples/capture_images_by_key.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+# Capture a PNG image while still running in the preview mode.
+# The image release is activated with the button 'p' and with 'q' the script is stopped.
+
+from picamera2 import Picamera2, Preview
+from sys import stdin
+from keyboard import is_pressed
+from termios import tcflush, TCIOFLUSH
+from time import strftime
+
+key_flag = False  
+cam = Picamera2()
+cam.start_preview(Preview.QTGL)
+cam.start()
+
+try:
+    while True:
+        if is_pressed('p'):
+            if key_flag == False:
+                key_flag = True
+            filename =  strftime("%Y%m%d-%H%M%S") + '.png'
+            cam.capture_file(filename, format="png", wait=None)
+            print("\rCaptured {} succesfully".format(filename))
+        else:
+            key_flag = 0
+        if is_pressed('q'):
+            print("\rClosing camera...")
+            break
+finally:
+    cam.stop_preview()
+    cam.stop()
+    cam.close()
+    tcflush(stdin, TCIOFLUSH)
