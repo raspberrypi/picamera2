08cddd02c57819c3bcaf7879825edc7c6f89191e
David Plowman <david.plowman@raspberrypi.com>
2023-05-04 09:58:04 +0100

    Convert tuples of libcamera.Rectangles to non-libcamera types
    Previously it was converting lists of them, but not tuples.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- picamera2/utils.py
+++ picamera2/utils.py
@@ -8,7 +8,7 @@ def convert_from_libcamera_type(value):
         value = (value.x, value.y, value.width, value.height)
     elif isinstance(value, Size):
         value = (value.width, value.height)
-    elif isinstance(value, list) and all(isinstance(item, Rectangle) for item in value):
+    elif isinstance(value, (list, tuple)) and all(isinstance(item, Rectangle) for item in value):
         value = [(v.x, v.y, v.width, v.height) for v in value]
     return value
 
