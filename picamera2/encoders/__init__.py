7b9e76a4d0c2ba2c5131ef60994f3673dd485409
David Plowman <david.plowman@raspberrypi.com>
2023-05-22 16:17:38 +0100

    encoders: Use libav transparently for h.264 when h/w encpder not available

--- picamera2/encoders/__init__.py
+++ picamera2/encoders/__init__.py
@@ -1,5 +1,20 @@
+import fcntl
+import v4l2
+
+_h264_hw_encoder_available = False
+try:
+    with open('/dev/video11', 'rb', buffering=0) as fd:
+        caps = v4l2.v4l2_capability()
+        fcntl.ioctl(fd, v4l2.VIDIOC_QUERYCAP, caps)
+        _hw_encoder_available = (caps.card.decode('utf-8') == "bcm2835-codec-encode")
+except Exception as e:
+    pass
+
 from .encoder import Encoder, Quality
-from .h264_encoder import H264Encoder
+if _h264_hw_encoder_available:
+    from .h264_encoder import H264Encoder
+else:
+    from .libav_encoder import LibavEncoder as H264Encoder
 from .jpeg_encoder import JpegEncoder
 from .mjpeg_encoder import MJPEGEncoder
 from .multi_encoder import MultiEncoder
