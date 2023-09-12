3d713c35c225596f2de3d57656558cc910e10c90
David Plowman <david.plowman@raspberrypi.com>
2023-09-12 13:57:49 +0100

    Allow pre-callback to mark frames as not for display
    The pre-callback can set request.display to False, in which case
    that request (the images within it) won't be rendered. This makes
    it easy for an application to "hide" a few frames.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- picamera2/picamera2.py
+++ picamera2/picamera2.py
@@ -1206,6 +1206,7 @@ class Picamera2:
             if requests:
                 display_request = requests[-1]
                 display_request.acquire()
+                display_request.display = True  # display requests by default
 
             if self.pre_callback:
                 for req in requests:
@@ -1243,7 +1244,7 @@ class Picamera2:
         # then we don't want it going back to the application as the memory is not valid.
         if display_request is not None:
             if display_request.configure_count == self.configure_count and \
-               display_request.config['display'] is not None:
+               display_request.config['display'] is not None and display_request.display:
                 display.render_request(display_request)
             display_request.release()
 
