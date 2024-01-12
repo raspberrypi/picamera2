6cdf00a556196332f636b21761fe0ceb9fc8efc8
unknown <gabriel.grunberg@gmail.com>
2024-01-12 22:30:29 +0100

    Changes to update overlay image in separate thread

--- examples/overlay_png.py
+++ examples/overlay_png.py
@@ -2,27 +2,12 @@ from picamera2.encoders import H264Encoder
 from picamera2 import Picamera2, MappedArray
 import cv2
 import time
+import threading
 
 def apply_overlay(request):
     """pre_callback function that overlays the image on the video 
-    We also re-load the image every OVERLAY_UPDATE_DELAY seconds 
-    This means if some other process were to put a new image in the same place it would automatically get updated while the camera is running
     """
-    
-    # since the pre_callback can not new inputs every time it's used it has to use the global variables
-    global OVERLAY_UPDATE_TIME
-    global OVERLAY_IMAGE
-    global OVERLAY_WIDTH
-    global OVERLAY_HEIGHT 
-    global OVERLAY_COLOR
-    global OVERLAY_ALPHA
-    global OVERLAY_UPDATE_DELAY
-    
-    # if it's been more than OVERLAY_UPDATE_DELAY seconds since last time we updated the image we update it again
-    if time.time() - OVERLAY_UPDATE_TIME > OVERLAY_UPDATE_DELAY:
-        OVERLAY_IMAGE, OVERLAY_WIDTH, OVERLAY_HEIGHT, OVERLAY_COLOR, OVERLAY_ALPHA = get_overlay()
-        OVERLAY_UPDATE_TIME = time.time()
-    
+ 
     with MappedArray(request, "main") as m:
         
         frame = m.array
@@ -32,36 +17,57 @@ def apply_overlay(request):
                 OVERLAY_ALPHA * OVERLAY_COLOR[:, :, c] + (1 - OVERLAY_ALPHA) * frame[0:OVERLAY_HEIGHT, 0:OVERLAY_WIDTH, c]
             )
             
-def get_overlay():
+def update_overlay():
     """ gets the overlay image and resizes it if needed to fit the video """
-    overlay_image = cv2.imread(OVERLAY_IMAGE_PATH, cv2.IMREAD_UNCHANGED)
-    overlay_height, overlay_width = overlay_image.shape[:2] # for some reason height first in the array
-    resize_overaly = False
-    if overlay_width > WIDTH:
-        overlay_width = WIDTH
-        resize_overaly = True
     
-    if overlay_height > HEIGHT:
-        overlay_height = HEIGHT
-        resize_overaly = True
+    global OVERLAY_IMAGE
+    global OVERLAY_WIDTH
+    global OVERLAY_HEIGHT
+    global OVERLAY_COLOR
+    global OVERLAY_ALPHA
+    
+    while True:
+        overlay_image = cv2.imread(OVERLAY_IMAGE_PATH, cv2.IMREAD_UNCHANGED)
+        overlay_height, overlay_width = overlay_image.shape[:2] # for some reason height first in the array
+        resize_overaly = False
+        if overlay_width > WIDTH:
+            overlay_width = WIDTH
+            resize_overaly = True
         
-    if resize_overaly:
-        overlay_image = cv2.resize(overlay_image, (overlay_width, overlay_height))
+        if overlay_height > HEIGHT:
+            overlay_height = HEIGHT
+            resize_overaly = True
+            
+        if resize_overaly:
+            overlay_image = cv2.resize(overlay_image, (overlay_width, overlay_height))
 
-    overlay_color = overlay_image[:, :, :3]
-    overlay_alpha = overlay_image[:, :, 3] / 255.0            
-    if overlay_image.shape[2] == 4:  # If the image has 4 channels
-        overlay_color = cv2.cvtColor(overlay_color, cv2.COLOR_BGR2RGB)     
-        
-    return overlay_image, overlay_width, overlay_height, overlay_color, overlay_alpha
+        overlay_color = overlay_image[:, :, :3]
+        overlay_alpha = overlay_image[:, :, 3] / 255.0            
+        if overlay_image.shape[2] == 4:  # If the image has 4 channels
+            overlay_color = cv2.cvtColor(overlay_color, cv2.COLOR_BGR2RGB)     
+            
+        OVERLAY_IMAGE, OVERLAY_WIDTH, OVERLAY_HEIGHT, OVERLAY_COLOR, OVERLAY_ALPHA = (
+            overlay_image,
+            overlay_width,
+            overlay_height,
+            overlay_color,
+            overlay_alpha
+        )
+        time.sleep(OVERLAY_UPDATE_DELAY)
      
 
 WIDTH = 1920
 HEIGHT = 1080
-OVERLAY_UPDATE_DELAY = 10
 OVERLAY_IMAGE_PATH = "/path/to/image.png"                 
-OVERLAY_IMAGE, OVERLAY_WIDTH, OVERLAY_HEIGHT, OVERLAY_COLOR, OVERLAY_ALPHA = get_overlay()
-OVERLAY_UPDATE_TIME = time.time()
+OVERLAY_IMAGE = None
+OVERLAY_WIDTH = None
+OVERLAY_HEIGHT = None
+OVERLAY_COLOR = None
+OVERLAY_ALPHA = None
+OVERLAY_UPDATE_DELAY = 1
+
+update_overlay_thread = threading.Thread(target=update_overlay)
+update_overlay_thread.start()
 
 
 picam2 = Picamera2()
