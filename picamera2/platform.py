39ae89b776be061f2ba18cf4479248e8df5b8661
Naushir Patuck <naush@raspberrypi.com>
2024-07-04 13:53:25 +0100

    More robust platform checking
    Increase the range of device nodes to search for a known device as
    they may be assigned automatically by the kernel probe order.
    
    Additionally, switch from using the CFE driver to the Backend driver
    when identifying a platform as the latter now has a fixed UAPI, and
    will always be present even if a sensor probe fails.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- picamera2/platform.py
+++ picamera2/platform.py
@@ -12,17 +12,17 @@ class Platform(Enum):
 
 _platform = Platform.VC4
 try:
-    for num in range(5):
+    for num in range(64):
         device = '/dev/video' + str(num)
         if os.path.exists(device):
             with open(device, 'rb+', buffering=0) as fd:
                 caps = v4l2.v4l2_capability()
                 fcntl.ioctl(fd, v4l2.VIDIOC_QUERYCAP, caps)
                 decoded = caps.card.decode('utf-8')
-                if decoded == "rp1-cfe":
+                if decoded == "pispbe":
                     _platform = Platform.PISP
                     break
-                elif decoded == "unicam":
+                elif decoded == "bcm2835-isp":
                     break
 except Exception:
     pass
