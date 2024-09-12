ece85c8e0141ffc876137025d503c96cdc5bb2df
Naushir Patuck <naush@raspberrypi.com>
2024-09-12 14:53:00 +0100

    Add back pose demo

---
+++ examples/imx500/imx500_pose_estimation_higherhrnet_demo.py
@@ -0,0 +1,87 @@
+import argparse
+import time
+
+import numpy as np
+
+from picamera2 import CompletedRequest, MappedArray, Picamera2
+from picamera2.devices.imx500 import IMX500
+from picamera2.devices.imx500.postprocess import COCODrawer
+from picamera2.devices.imx500.postprocess_highernet import \
+    postprocess_higherhrnet
+
+last_boxes = None
+last_scores = None
+last_keypoints = None
+WINDOW_SIZE_H_W = (480, 640)
+
+
+def ai_output_tensor_parse(metadata: dict):
+    """Parse the output tensor into a number of detected objects, scaled to the ISP out."""
+    global last_boxes, last_scores, last_keypoints
+    np_outputs = imx500.get_outputs(metadata=metadata, add_batch=True)
+    if np_outputs is not None:
+        keypoints, scores, boxes = postprocess_higherhrnet(outputs=np_outputs,
+                                                           img_size=WINDOW_SIZE_H_W,
+                                                           img_w_pad=(0, 0),
+                                                           img_h_pad=(0, 0),
+                                                           detection_threshold=args.detection_threshold,
+                                                           network_postprocess=True)
+
+        if scores is not None and len(scores) > 0:
+            last_keypoints = np.reshape(np.stack(keypoints, axis=0), (len(scores), 17, 3))
+            last_boxes = [np.array(b) for b in boxes]
+            last_scores = np.array(scores)
+    return last_boxes, last_scores, last_keypoints
+
+
+def ai_output_tensor_draw(request: CompletedRequest, boxes, scores, keypoints, stream='main'):
+    """Draw the detections for this request onto the ISP output."""
+    with MappedArray(request, stream) as m:
+        if boxes is not None and len(boxes) > 0:
+            drawer.annotate_image(m.array, boxes, scores,
+                                  np.zeros(scores.shape), keypoints, args.detection_threshold,
+                                  args.detection_threshold, request.get_metadata(), picam2, stream)
+
+
+def picamera2_pre_callback(request: CompletedRequest):
+    """Analyse the detected objects in the output tensor and draw them on the main output image."""
+    boxes, scores, keypoints = ai_output_tensor_parse(request.get_metadata())
+    ai_output_tensor_draw(request, boxes, scores, keypoints)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, help="Path of the model",
+                        default="/usr/share/imx500-models/imx500_network_higherhrnet_coco.rpk")
+    parser.add_argument("--fps", type=int, default=10, help="Frames per second")
+    parser.add_argument("--detection-threshold", type=float, default=0.3,
+                        help="Post-process detection threshold")
+    parser.add_argument("--labels", type=str, default="assets/coco_labels.txt",
+                        help="Path to the labels file")
+    return parser.parse_args()
+
+
+def get_drawer():
+    with open(args.labels, "r") as f:
+        categories = f.read().split("\n")
+    categories = [c for c in categories if c and c != "-"]
+    return COCODrawer(categories, imx500, needs_rescale_coords=False)
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    # This must be called before instantiation of Picamera2
+    imx500 = IMX500(args.model)
+    drawer = get_drawer()
+
+    picam2 = Picamera2(imx500.camera_num)
+    config = picam2.create_preview_configuration(controls={'FrameRate': args.fps}, buffer_count=12)
+
+    imx500.show_network_fw_progress_bar()
+    picam2.start(config, show_preview=True)
+    imx500.set_auto_aspect_ratio()
+    picam2.pre_callback = picamera2_pre_callback
+
+    while True:
+        time.sleep(0.5)
