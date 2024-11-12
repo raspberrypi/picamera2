fdef49c2268a296c74b47579b7b910a57da411d8
Naushir Patuck <naush@raspberrypi.com>
2024-11-12 09:50:43 +0000

    tests: crops: Reduce the size of the lores image
    Due to a rendering bug, if the lores viewfinder covers the main
    viewfinder, updates stall for an unknown reason. Avoid this temporarily
    by reducing the preview image resolution.  Also lower the test time
    by reducing the number of frames run in each config.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- tests/crop_test.py
+++ tests/crop_test.py
@@ -15,15 +15,14 @@ picam2 = Picamera2()
 
 for m, l in [(False, False), (False, True), (True, False), (True, True)]:
     cfg = picam2.create_video_configuration(main={"size": (1920, 1080), "format": 'XRGB8888', "preserve_ar": m},
-                                            lores={"size": (640, 640), "format": 'XRGB8888', "preserve_ar": l},
+                                            lores={"size": (320, 320), "format": 'XRGB8888', "preserve_ar": l},
                                             display="main")
     picam2.configure(cfg)
     picam2.start(show_preview=True)
 
-    for _ in range(100):
+    for _ in range(50):
         im = picam2.capture_array("lores")
         cv2.imshow("lores", im)
-        cv2.resizeWindow("lores", (640, 640))
         cv2.waitKey(1)
 
     picam2.stop()
