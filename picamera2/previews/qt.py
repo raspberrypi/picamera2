423d40e477716d3c12886d017ead8976cceeb637
Levi Dean <leviodean@gmail.com>
2025-02-12 13:11:39 +0000

    Add compatibility with PySide6 and PyQt6 for QGlPicamera2 (QGl6Picamera2, and QGlSide6Picamera2 respectively)
    Signed-off-by: Levi Dean <leviodean@gmail.com>

--- picamera2/previews/qt.py
+++ picamera2/previews/qt.py
@@ -5,20 +5,30 @@
 # which is in any case what is required for remote preview windows.
 from logging import getLogger
 
-from .q_picamera2 import _get_qpicamera2
 from .qt_compatability import _QT_BINDING
+from .q_picamera2 import _get_qpicamera2
 
+_log = getLogger(__name__)
+
+try:
+    from .q_gl_picamera2 import _get_qglpicamera2
+except Exception:
+    _log.warning("OpenGL will not be available")
+
+
+# Lazy load QPicamera2 widget classes as will likely only use one or two within a given application
 def __getattr__(name: str):
+    # Standard Qt widgets
     if name == 'QPicamera2':
         return _get_qpicamera2(_QT_BINDING.PyQt5)
     elif name == 'Q6Picamera2':
         return _get_qpicamera2(_QT_BINDING.PyQt6)
     elif name == 'QSide6Picamera2':
         return _get_qpicamera2(_QT_BINDING.PySide6)
-
-_log = getLogger(__name__)
-
-try:
-    from .q_gl_picamera2 import QGlPicamera2
-except Exception:
-    _log.warning("OpenGL will not be available")
+    # OpenGL accelerated Qt widgets
+    elif name == 'QGlPicamera2':
+        return _get_qglpicamera2(_QT_BINDING.PyQt5)
+    elif name == 'QGl6Picamera2':
+        return _get_qglpicamera2(_QT_BINDING.PyQt6)
+    elif name == 'QGlSide6Picamera2':
+        return _get_qglpicamera2(_QT_BINDING.PySide6)
