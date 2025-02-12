fa4a7b3534578ec8fe56698432106dbfa5591e0a
Levi Dean <leviodean@gmail.com>
2025-02-12 00:02:11 +0000

    Add compatibility with PySide6 and PyQt6 for QPicamera2 (Q6Picamera2, and QSide6Picamera2 respectively)
    Signed-off-by: Levi Dean <leviodean@gmail.com>

---
+++ picamera2/previews/qt_compatability.py
@@ -0,0 +1,40 @@
+from types import ModuleType
+from enum import Enum
+import importlib
+
+class _QT_BINDING(Enum):
+    PyQt5 = 'PyQt5'
+    PyQt6 = 'PyQt6'
+    PySide6 = 'PySide6'
+
+def _get_qt_modules(qt_module: _QT_BINDING) -> tuple[ModuleType, ModuleType, ModuleType]:
+    """
+    Gets the qt modules for the given qt binding
+
+    Parameters
+    ----------
+    qt_module : _QT_BINDING
+        The module we want to get Qt QtCore, QtGui, and QtWidgets modules from
+
+    Returns
+    -------
+    out : tuple[ModuleType, ModuleType, ModuleType]
+        A tuple containing the QtCore, QtGui, and QtWidgets modules respectively, from the given python qt module
+
+    Notes
+    -----
+    - Assigns alternate attributes for Signals and Slots as these are different in PySide and PyQt
+    """
+    try:
+        QtCoreModule = importlib.import_module('.QtCore', package=qt_module.value)
+        QtGuiModule = importlib.import_module('.QtGui', package=qt_module.value)
+        QtWidgetsModule = importlib.import_module('.QtWidgets', package=qt_module.value)
+
+        if qt_module == _QT_BINDING.PySide6:
+            QtCoreModule.pyqtSignal = QtCoreModule.Signal
+            QtCoreModule.pyqtSlot = QtCoreModule.Slot
+
+        return (QtCoreModule, QtGuiModule, QtWidgetsModule)
+    except ImportError:
+        raise ImportError(f'{qt_module} is not installed or could not be imported.')
+
