raspberrypi/picamera2

commit c0cc32c32646bf3341a4e05a32dfea747b065bf6

Always sync buffers as writeable when the request syncs them
This makes the CompletedRequest always sync writes to its buffers
so the extra sync can be removed from _MappedBuffer.

Signed-off-by: Peter Bailey <peter.bailey@raspberrypi.com>

picamera2/request.py
@@ -33,14 +33,16 @@ class _MappedBuffer:
             stream = request.stream_map[stream]
         assert request.request is not None
         self.__fb = request.request.buffers[stream]
-        self.__sync = request.picam2.allocator.sync(request.picam2.allocator, self.__fb, write)
+        self.__allocator = request.picam2.allocator
 
     def __enter__(self) -> Any:
-        self.__mm = self.__sync.__enter__()
+        self.__mm = self.__allocator.mapped_buffers.get(self.__fb, None)
+        if self.__mm is None:
+            raise RuntimeError("failed to find buffer")
         return self.__mm
 
     def __exit__(self, exc_type: Any, exc_value: Any, exc_traceback: Any) -> None:
-        self.__sync.__exit__(exc_type, exc_value, exc_traceback)
+        pass
 
 
 class MappedArray:
@@ -90,7 +92,7 @@ class CompletedRequest:
         self.config = self.picam2.camera_config.copy()
         self.stream_map = self.picam2.stream_map.copy()
         with self.lock:
-            self.syncs = [picam2.allocator.sync(self.picam2.allocator, buffer, False)
+            self.syncs = [picam2.allocator.sync(self.picam2.allocator, buffer, True)
                           for buffer in self.request.buffers.values()]
             self.picam2.allocator.acquire(self.request.buffers)
             [sync.__enter__() for sync in self.syncs]

picamera2/allocators/dmaallocator.py
@@ -129,3 +129,9 @@ class DmaAllocator(Allocator):
             ret = fcntl.ioctl(self.__fb.planes[0].fd, DMA_BUF_IOCTL_SYNC, dma_sync)
             if ret:
                 logging.error("failed to unlock-sync-write dma buf")
+
+        def get_buffer(self):
+            it = self.allocator.mapped_buffers.get(self.__fb, None)
+            if it is None:
+                raise RuntimeError("failed to find buffer in DmaSync")
+            return it

commit d22e11e72705ac8e49bc1f4ffdc8edacf4b8f779

Fix use of MappedArray with LibcameraAllocator
This was broken by commit c0cc32c ("Always sync buffers as
writeable ...").

But of course LibcameraAllocator is only a legacy thing now, so you
really shouldn't be using it.

Although the code here is now behaving as it did previously, it is
rather unoptimised in that every MappedArray will do another mmap
which could probably be avoided. But you won't get the
LibcameraAllocator by default, and as stated, there is no reason to
request one - so it doesn't seem worth the trouble of doing anything
about it.

Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

picamera2/allocators/allocator.py
@@ -1,6 +1,9 @@
 class Allocator:
     """Base class for allocators"""
 
+    # Most allocators shouldn't need to "sync" buffers every time you access them.
+    needs_sync = False
+
     def __init__(self):
         self.sync = Sync
 

picamera2/allocators/libcameraallocator.py
@@ -10,6 +10,9 @@ _log = logging.getLogger("picamera2")
 class LibcameraAllocator(Allocator):
     """Uses the libcamera FrameBufferAllocator"""
 
+    # This legacy allocator does require a sync, but of course you shouldn't be using it.
+    needs_sync = True
+
     def __init__(self, camera):
         super().__init__()
         self.camera = camera

picamera2/request.py
@@ -34,15 +34,25 @@ class _MappedBuffer:
         assert request.request is not None
         self.__fb = request.request.buffers[stream]
         self.__allocator = request.picam2.allocator
+        self.__sync = None
+        if self.__allocator.needs_sync:
+            # This is just for legacy LibcameraAllocator, which you shouldn't really use.
+            self.__sync = self.__allocator.sync(self.__allocator, self.__fb, write)
 
     def __enter__(self) -> Any:
-        self.__mm = self.__allocator.mapped_buffers.get(self.__fb, None)
+        if self.__sync:
+            # For legacy LibcameraAllocator, which you shouldn't be using.
+            self.__mm = self.__sync.__enter__()
+        else:
+            self.__mm = self.__allocator.mapped_buffers.get(self.__fb, None)
         if self.__mm is None:
             raise RuntimeError("failed to find buffer")
         return self.__mm
 
     def __exit__(self, exc_type: Any, exc_value: Any, exc_traceback: Any) -> None:
-        pass
+        if self.__sync:
+            # For legacy LibcameraAllocator, which you shouldn't be using.
+            self.__sync.__exit__(exc_type, exc_value, exc_traceback)
 
 
 class MappedArray:

tests/allocator_test.py
@@ -4,7 +4,7 @@
 
 import time
 
-from picamera2 import Picamera2, Preview
+from picamera2 import MappedArray, Picamera2, Preview
 from picamera2.allocators import LibcameraAllocator
 
 picam2 = Picamera2()
@@ -16,6 +16,11 @@ picam2.configure(preview_config)
 
 picam2.start()
 time.sleep(2)
+
+# Check that MappedArray still works!
+with picam2.captured_request() as request:
+    with MappedArray(request, 'main') as m:
+        pass
 
 size = picam2.sensor_resolution
 # GPU won't digest images wider than 4096 on a Pi 4.