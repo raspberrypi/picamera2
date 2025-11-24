73d5495d89415e5f3c39e1e02242387d5943455e
David Plowman <david.plowman@raspberrypi.com>
2025-11-24 11:15:24 +0000

    Replace tflite import with ai_edge_litert
    Which now (finally!) exists for Python 3.13
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- examples/tensorflow/compositing.py
+++ examples/tensorflow/compositing.py
@@ -9,7 +9,7 @@ import time
 
 import cv2
 import numpy as np
-import tflite_runtime.interpreter as tflite
+from ai_edge_litert.interpreter import Interpreter
 from PIL import Image
 
 from picamera2 import MappedArray, Picamera2, Preview
@@ -52,7 +52,7 @@ def InferenceTensorFlow(image, model, label=None):
     else:
         labels = None
 
-    interpreter = tflite.Interpreter(model_path=model, num_threads=4)
+    interpreter = Interpreter(model_path=model, num_threads=4)
     interpreter.allocate_tensors()
 
     input_details = interpreter.get_input_details()
