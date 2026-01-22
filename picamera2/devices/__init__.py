9a47625ea95ea6e6a7132087415fc11b3d6f7ed4
Naushir Patuck <naush@raspberrypi.com>
2026-01-22 12:43:11 +0000

    devices: hailo: Add a helper to determine the Hailo device architecture
    This will be needed in a future commit to auto-select the correct model
    HEF files for the detect and pose examples.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- picamera2/devices/__init__.py
+++ picamera2/devices/__init__.py
@@ -1,6 +1,6 @@
 try:
     # Hailo requires hailo_platform package, which may not be installed on non-Hailo platforms.
-    from .hailo import Hailo
+    from .hailo import Hailo, hailo_architecture
 except ModuleNotFoundError:
     pass
 from .imx500 import IMX500
